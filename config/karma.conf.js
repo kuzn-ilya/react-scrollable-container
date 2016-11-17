085f33220ba332d40350fe997cba2754934b4dfc
b-52 <kuzn.ilya@gmail.com>
2016-11-17 19:59:20 +0300

    karma launcher for Chrome was added

--- config/karma.conf.js
+++ config/karma.conf.js
@@ -43,7 +43,7 @@ module.exports = function (config) {
     colors: true,
     logLevel: config.LOG_INFO,
     autoWatch: true,
-    browsers: ['PhantomJS'],
+    browsers: ['PhantomJS', 'Chrome'],
     singleRun: true,
     concurrency: Infinity
   })

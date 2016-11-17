89c0e96cc1d592bd44332991828968f2aa92f84c
b-52 <kuzn.ilya@gmail.com>
2016-11-17 21:16:23 +0300

    test for rendering a child (does not pass)

--- test/container-scrollable.dom.spec.tsx
+++ test/container-scrollable.dom.spec.tsx
@@ -31,4 +31,33 @@ describe('DOM: ContainerScrollable', () => {
         expect(element).to.have.property('offsetTop', 40);
     });
 
+    it('!!!', () => {
+        document.body.style.height = '100%';
+        document.body.style.width = '100%';
+        document.body.style.margin = '0px';
+
+        let div = document.createElement('div');
+        div.style.height = '100%';
+        div.style.width = '100%';
+
+        document.body.appendChild(div);
+
+        ReactDOM.render(
+            <ContainerScrollable id="container" style={{left: "40px", top: "40px", width: "200px", height: "200px"}}
+                overflowX="auto" overflowY="auto">
+                <div style={{top: "260px", left: "280px", width: "20px", height: "20px", display: "inline-block", position: "absolute"}} />
+            </ContainerScrollable>,
+            div);
+
+        let element = document.body.querySelector('#container');
+        expect(element).to.exist;
+
+        console.log(element);
+
+        expect(element).to.have.property('offsetWidth', 300);
+        expect(element).to.have.property('offsetHeight', 280);
+        expect(element).to.have.property('offsetLeft', 40);
+        expect(element).to.have.property('offsetTop', 40);
+    });
+
 });

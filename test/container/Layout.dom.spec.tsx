21ca82883e1ccd0b14eb92e7c7453333bc625421
b-52 <kuzn.ilya@gmail.com>
2016-12-10 19:38:20 +0300

    yet another test for Layout

--- test/container/Layout.dom.spec.tsx
+++ test/container/Layout.dom.spec.tsx
@@ -1,11 +1,11 @@
-//import * as React from 'react';
-//import * as ReactDOM from 'react-dom';
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
 import * as chai from 'chai';
 import * as chaiSpies from 'chai-spies';
 
-//import { Layout } from '../../sources/container/Layout';
+import { Layout } from '../../sources/container/Layout';
 
-//const expect = chai.expect;
+const expect = chai.expect;
 chai.use(chaiSpies);
 
 describe('DOM: Layout', () => {
@@ -38,5 +38,57 @@ describe('DOM: Layout', () => {
     afterEach(() => {
         document.head.removeChild(style);
         document.body.removeChild(div);
+    });
+
+    it('should have children with expected heights', () => {
+        ReactDOM.render(
+                <Layout
+                    width = "200px"
+                    height = "200px"
+                    firstChildHeight = "10px"
+                >
+                    <div>
+                        <div style= {{display: 'block', height: '100%', width: '100%'}}/>
+                    </div>
+                    <div>
+                        <div style= {{display: 'block', height: '100%', width: '100%'}}/>
+                    </div>
+                </Layout>,
+            div);
+
+        expect(div.children).to.exist;
+        expect(div.children.length).to.be.equal(1);
+        let container = div.children[0] as HTMLElement;
+        expect(container).to.exist;
+
+        expect(container.offsetWidth).to.be.equal(200);
+        expect(container.offsetHeight).to.be.equal(200);
+
+        expect(container.children).to.exist;
+        expect(container.children.length).to.be.equal(2);
+
+        let first = container.children[0] as HTMLElement;
+        expect(first).to.exist;
+        expect(first.offsetHeight).to.be.equal(10);
+
+        expect(first.children).to.exist;
+        expect(first.children.length).to.be.equal(1);
+
+        // let firstDiv = first.children[0] as HTMLElement;
+        // expect(firstDiv).to.exist;
+
+        // expect(firstDiv.offsetHeight).to.be.equal(10);
+
+        let second = container.children[1] as HTMLElement;
+        expect(second).to.exist;
+        expect(second.offsetHeight).to.be.equal(190);
+
+        expect(second.children).to.exist;
+        expect(second.children.length).to.be.equal(1);
+
+        // let secondDiv = second.children[0] as HTMLElement;
+        // expect(secondDiv).to.exist;
+
+        // expect(secondDiv.offsetHeight).to.be.equal(190);
     });
 });

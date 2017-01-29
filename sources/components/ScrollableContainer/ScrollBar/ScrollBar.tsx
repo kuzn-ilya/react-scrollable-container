922d5195246a59338414ea2c0b5712ad14fac0f0
b-52 <kuzn.ilya@gmail.com>
2017-01-29 18:36:34 +0300

    [ScrollBar]: mouse up/down handlers added

--- sources/components/ScrollableContainer/ScrollBar/ScrollBar.tsx
+++ sources/components/ScrollableContainer/ScrollBar/ScrollBar.tsx
@@ -13,6 +13,9 @@ export class ScrollBar extends React.PureComponent<ScrollBarProps, Partial<Scrol
         this.prevButtonClick = this.prevButtonClick.bind(this);
         this.nextButtonClick = this.nextButtonClick.bind(this);
 
+        this.handleMouseDown = this.handleMouseDown.bind(this);
+        this.handleMouseUp = this.handleMouseUp.bind(this);
+
         this.state = this.calculateSize(0, this.props);
     }
 
@@ -36,19 +39,35 @@ export class ScrollBar extends React.PureComponent<ScrollBarProps, Partial<Scrol
     }
 
     prevButtonClick: () => void = () => {
-        if (this.state.position > this.props.min) {
-            this.setState({
-                position: this.state.position - 1
-            });
-        }
+        this.moveBy(-this.props.smallChange);
     }
 
     nextButtonClick: () => void = () => {
-        if (this.state.position < this.props.max) {
-            this.setState({
-                position: this.state.position + 1
-            });
+        this.moveBy(this.props.smallChange);
+    }
+
+    handleMouseDown: () => void = () => {
+        return;
+    }
+
+    handleMouseUp: () => void = () => {
+        return;
+    }
+
+    moveBy(delta: number): void {
+        let newPosition = this.state.position + delta;
+
+        if (delta < 0 && newPosition < this.props.min) {
+            newPosition = this.props.min;
         }
+
+        if (delta > 0 && newPosition > this.props.max) {
+            newPosition = this.props.max;
+        }
+
+        this.setState({
+            position: newPosition
+        });
     }
 
     updateState(props: ScrollBarProps, position?: number): void {
@@ -93,6 +112,8 @@ export class ScrollBar extends React.PureComponent<ScrollBarProps, Partial<Scrol
                     width: this.props.width
                 }}
                 className="scrollbar-container"
+                onMouseDown={this.handleMouseDown}
+                onMouseUp={this.handleMouseUp}
             >
                 <ScrollBarButton
                     type={this.props.orientation === 'vertical' ? 'top' : 'left'}
